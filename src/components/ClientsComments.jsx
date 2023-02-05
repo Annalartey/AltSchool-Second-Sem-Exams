93237069adc677d77a985342c5084e0426c6be0e
Anna Lartey <anna.t.lartey@gmail.com>
2023-02-05 14:54:20 +0000

    Mapped through names of users in comments section

--- src/components/ClientsComments.jsx
+++ src/components/ClientsComments.jsx
@@ -2,11 +2,48 @@ import React from 'react';
 
 
 export default function ClientComments () {
+  const data = [
+    {
+      name: "Anna",
+      image: "imgurl",
+      comment: "i haven't seen any learning app as simple as Learn",
+    },
+    {
+      name: "Blessing",
+      image: "imgurl",
+      comment: "i haven't seen any learning app as simple as Learn",
+    },
+    {
+      name: "David",
+      image: "imgurl",
+      comment: "i haven't seen any learning app as simple as Learn",
+    },
+    
+    {
+      name: "Vicky",
+      image: "imgurl",
+      comment: "i haven't seen any learning app as simple as Learn",
+    },
+    {
+      name: "Sharron Wehns",
+      image: "imgurl",
+      comment: "i haven't seen any learning app as simple as Learn",
+    },
+  ]
   return (
     <>
       <div> 
     <h1> What clients say about us </h1>
-    
+    <div>
+      {
+        data.map((d)=>{
+          return (
+<p> {d.name}</p>
+            )
+          }
+)
+      }
+    </div>
   </div>
     </>
     )
