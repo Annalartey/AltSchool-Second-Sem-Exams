b88005744171d3cc2da6a4e2d65ce463e2c1af49
Anna Lartey <anna.t.lartey@gmail.com>
2023-02-09 18:39:20 +0000

    mapped lessons in grades

--- src/components/Lessons.jsx
+++ src/components/Lessons.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import {Link, Outlet} from 'react-router-dom'
 
 function Lessons() {
+  data = [
+    {grade: "One",
+    star: "*",
+    link:""},
+    {grade: "Two",
+    star: "**",
+    link:""},
+    {grade: "Three",
+    star: "***",
+    link:""},
+    {grade: "Four",
+    star: "****",
+    link:""},
+    {grade: "Five",
+    star: "*****",
+    link:""},
+    {grade: "Six",
+    star: "******",
+    link:""},
+  ]
   return (
     <div>
         <h1>Lessons</h1>
@@ -9,6 +29,19 @@ function Lessons() {
         <Link to = '/dashboard/lesson/grade_1'>grade 1</Link>
         <Link to = '/dashboard/lesson/grade_1'>grade 2</Link>
         <Link to = '/dashboard/lesson/grade_1'>grade 3</Link>
+      </div>
+
+      <div>
+        {
+          data.map((d) => {
+            return(
+              <Link to = {d.link}>
+                <p> Grade {d.grade} Lesson</p>
+                <p>{d.star}</p>
+              </Link>
+            )
+          })
+        }
       </div>
         
         <Outlet/>
