3ebc17cb7ff04f3e83d7e28aa73d631867f92e14
Anna Lartey <anna.t.lartey@gmail.com>
2023-02-12 10:08:10 +0000

    Added affiliate

---
+++ src/components/AffiliateLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AffiliateLogo () {
+  return (
+<div>
+hello affiliate schools
+  </div>
+    )
+  }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import {Helmet} from 'react-helmet-async'
 import {Link} from 'react-router-dom'
 import "../App.css"
 import ClientsComments from './ClientsComments'
+import AffiliateLogo from './AffiliateLogo'
 function Home() {
   return (
     <>
@@ -57,6 +58,7 @@ function Home() {
 
       </main>
           <ClientsComments/>
+        <AffiliateLogo/>
 
     </div>
 
