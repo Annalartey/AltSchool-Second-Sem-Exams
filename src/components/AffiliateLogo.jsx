b6d5c1cdc08e2afc76a5381a5576ff2e39e9ada0
Anna Lartey <anna.t.lartey@gmail.com>
2023-02-13 15:13:25 +0000

    update

--- src/components/AffiliateLogo.jsx
+++ src/components/AffiliateLogo.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
 export default function AffiliateLogo () {
+  const logo = [
+    {
+      name: "company",
+      pic: "image"
+    },
+     {
+      name: "company",
+      pic: "image"
+    },
+     {
+      name: "company",
+      pic: "image"
+    },
+     {
+      name: "company",
+      pic: "image"
+    },
+  ]
   return (
-<div>
-hello affiliate schools{}{{
+  <div>
+    hello affiliate schools
+    <div>
+      {
+        logo.map((l) => {
+          return(
+            <div>
+               l.name
+            </div>
+           
+          )
+        })
+      }
+    </div>
   </div>
     )
   }
